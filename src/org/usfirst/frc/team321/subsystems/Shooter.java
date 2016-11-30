0bb35765a1ad22aff38dd7eb4cd848acff840299
kevinkarnani <kevinkarnani@gmail.com>
2016-11-30 15:59:07 -0500

    Update Documentation for Shooter.java

--- src/org/usfirst/frc/team321/subsystems/Shooter.java
+++ src/org/usfirst/frc/team321/subsystems/Shooter.java
@@ -8,10 +8,11 @@ import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *Shooter class for the shooter of the robot which takes boulders
- *from the shooter motor and shoots it. It incorporates PID and commands
- *to guide it towards the high goal.
+ * Class for the shooter of the robot which takes boulders
+ * from the shooter motor and shoots it. It incorporates PID
+ * and commands to guide it towards the high goal.
  */
+
 public class Shooter extends Subsystem {
 	private SpeedController shooter;
 	private static final int MAX_RPM = 18730;
@@ -20,16 +21,18 @@ public class Shooter extends Subsystem {
 	private static final double WHEEL_RADIUS = 0.20;
 	
 	/**
-	 * Retrieves the shooter motor from the RobotMap
+	 * Constructs a Talon object, then retrieves the shooter motor from the RobotMap.
 	 */
+	
 	public Shooter(){
 		shooter = new Talon(RobotMap.SHOOTER);
 	}
 
 	/**
-	 * @param power value of the motor being checked
-	 * Used in UseShooter command
+	 * @param power Value of the motor being checked.
+	 * Used in UseShooter command.
 	 */
+	
 	public void setShooterLimit(double power){
 		shooter.set(MathUtil.clamp(power, -1.0, 1.0));
 	}
